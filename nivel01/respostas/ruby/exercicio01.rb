e155b61f653d6861125f4e57fc1d2d3c16ba780e
beregueder <beregueder@gmail.com>
2014-02-10 16:01:17 -0200

    Resolvendo os algoritimo com ruby e até exercicio3

---
+++ nivel01/respostas/ruby/exercicio01.rb
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+
+ab = a
+a = b
+b =ab
+
+
+puts "Valor A: ", a, " valor b: ",b
---
+++ nivel01/respostas/ruby/exercicio03.rb
@@ -0,0 +1,6 @@
+print "Digite a altura do retângulo: "
+altura = gets.to_i
+print "Digite a base do retângulo: "
+base =gets.to_i
+
+puts "Area do retângulo: ", altura * base
